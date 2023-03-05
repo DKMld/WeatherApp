87cd6c2bf69feb9fc409b9819da1a8376e9104e3
DKMld <miti_01@abv.bg>
2023-03-06 00:04:57 +0200

    'added_time_and_location'

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,27 +1,40 @@
+import math
+
 from django.shortcuts import render
 import requests
 import json
 
 
 def home_page(request):
-    weather_data = get_weather_data(request)
 
-    context = {'temp': weather_data['main']['temp'],
-               'description': weather_data['weather'][0]['main'],
-               }
+    if request.method == 'post':
+        ...
+
+
+    city = get_user_location()
+    weather_data = get_weather_data(request, city)
+    context = {
+        'temp': math.ceil(weather_data['main']['temp']),
+        'description': weather_data['weather'][0]['main'],
+        'city': city,
+    }
+
     return render(request, 'home_page.html', context)
 
 
-def get_weather_data(request):
-    user_current_city = get_user_location()
+def get_weather_data(request, city):
     api_key = 'd9cfcf4247b97e74578bb2a0f9e6cedf'
 
-    api_request = f'https://api.openweathermap.org/data/2.5/weather?q={user_current_city}&appid={api_key}&units=metric'
+    api_request = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
     response = requests.get(api_request)
 
     data = response.json()
     print(data)
     return data
+
+
+
+
 
 def get_ip():
     response = requests.get('https://api64.ipify.org?format=json').json()
