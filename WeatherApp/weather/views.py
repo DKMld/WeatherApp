63e01973707c7f3ad66681efd78f785e06309579
DKMld <miti_01@abv.bg>
2023-03-05 23:48:41 +0200

    'added_weather_template'

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -4,21 +4,24 @@ import json
 
 
 def home_page(request):
+    weather_data = get_weather_data(request)
 
-    return render(request, 'home_page.html')
+    context = {'temp': weather_data['main']['temp'],
+               'description': weather_data['weather'][0]['main'],
+               }
+    return render(request, 'home_page.html', context)
 
 
 def get_weather_data(request):
     user_current_city = get_user_location()
     api_key = 'd9cfcf4247b97e74578bb2a0f9e6cedf'
 
-    api_request = f'https://api.openweathermap.org/data/2.5/weather?q={user_current_city}&appid={api_key}'
-    response = request.get(api_request)
+    api_request = f'https://api.openweathermap.org/data/2.5/weather?q={user_current_city}&appid={api_key}&units=metric'
+    response = requests.get(api_request)
 
-    json_response = response.json()
-    formated_response = json_response.json.dumps(json_response)
-    
-    return formated_response
+    data = response.json()
+    print(data)
+    return data
 
 def get_ip():
     response = requests.get('https://api64.ipify.org?format=json').json()
