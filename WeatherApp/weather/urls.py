139bd5c7b9302f3bfae7f72145ccb599f955dc42
DKMld <miti_01@abv.bg>
2023-03-09 00:30:52 +0200

    'small_changes_in_the_template'

--- WeatherApp/weather/urls.py
+++ WeatherApp/weather/urls.py
@@ -5,5 +5,5 @@ from WeatherApp.weather import views as weather_views
 
 urlpatterns = [
     path('', weather_views.home_page, name='home_page'),
-    path('/search', weather_views.get_info_from_search, name='search'),
+    path('search', weather_views.get_info_from_search, name='search'),
 ]
--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -4,7 +4,6 @@ import requests
 
 
 def home_page(request):
-
     user_current_location = get_user_location()
     weather_data = get_weather_data(request, user_current_location['city'])
 
@@ -18,27 +17,31 @@ def home_page(request):
 
 
 def get_info_from_search(request):
+    try:
+        if request.method == 'GET':
+            city_input = request.GET.get('city_input')
+            weather_data = get_weather_data(request, city_input)
 
-    if request.method == 'GET':
-        city_input = request.GET.get('city_input')
-        weather_data = get_weather_data(request, city_input)
+            if len(city_input) > 0:
+                context = {
+                    'temp': math.ceil(weather_data['main']['temp']),
+                    'description': weather_data['weather'][0]['main'],
+                    'city': city_input,
+                }
+                return render(request, 'home_page.html', context)
+        return redirect('home_page')
 
-        if len(city_input) > 0:
-            context = {
-                'temp': math.ceil(weather_data['main']['temp']),
-                'description': weather_data['weather'][0]['main'],
-                'city': city_input,
-            }
-            return render(request, 'home_page.html', context)
-
+    except:
         return redirect('home_page')
 
 
 def get_weather_data(request, city):
     api_key = 'd9cfcf4247b97e74578bb2a0f9e6cedf'
 
-    api_request = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    api_request = f'https://api.openweathermap.org/data/2.5/weather?q={city}&exclude=hourly&appid={api_key}&units=metric'
     response = requests.get(api_request)
+    print(api_request)
+
 
     data = response.json()
 
